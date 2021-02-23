fd59383a6807b954d24b6a8cc31f1bfbfb6ab40a
AmaarJ <67553134+AmaarJ@users.noreply.github.com>
2021-02-24 00:20:29 +0300

    Add files via upload

---
+++ Flask Web App/website/models.py
@@ -0,0 +1,27 @@
+#creating the database models for users and notes
+from . import db
+#module helps to log users in and user objects needs to inherit from UserMixin
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+#foreign key is a column in a database that references a column in another database
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    #stores current date and time of note with note object
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    #foreign key ensures a valid id of an existing user is passed to this field.
+    #One (user) to many (notes) relationship. 
+    #reference the primary key of the user object -> id  
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    #unique identifier
+    id = db.Column(db.Integer, primary_key=True)
+    #no two users can have the same email
+    email = db.Column(db.String(150),unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    #stores all the notes that the user owns
+    notes = db.relationship('Note')
